4ad0260aab119775d71ef42271c3430db4da2b61
lobashev-andrey <lobashev.andrey@mail.ru>
2023-09-12 15:54:15 +0300

    added uploadFile() (2.6 final)

--- src/main/java/org/example/web/controllers/BookShelfController.java
+++ src/main/java/org/example/web/controllers/BookShelfController.java
@@ -12,8 +12,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 @Controller
 @RequestMapping(value = "/books")
@@ -64,5 +70,30 @@ public class BookShelfController {
              return "redirect:/books/shelf";
         }
     }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws Exception {
+        String name = file.getOriginalFilename(); // имя с расширением
+        byte[] bytes = file.getBytes();  // сохраняем сам файл в виде байтов
+        // теперь создаем - программно -
+        // каталог в домашней директории сервера
+        // на случай если его нет или он был удален
+        String rootPath = System.getProperty("catalina.home"); // задаем путь до папки сервера
+        File dir = new File(rootPath + File.separator + "external_uploads"); // имя папки - путь + имя
+        if(!dir.exists()){
+            dir.mkdirs(); // если каталог не существует, он будет создан
+        }
+        // создаем файлы, которые понадобятся
+        File servletFile = new File(dir.getAbsolutePath() + File.separator + name);
+        BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(servletFile));
+        stream.write(bytes);
+        stream.close();  // ВАЖНО (или использовать try-catch-with-resources)
+
+        logger.info("new file saved at: " + servletFile.getAbsolutePath());
+
+        return "redirect:/books/shelf";
+    }
+
+
 
 }
